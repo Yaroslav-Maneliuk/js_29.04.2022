91224d73dc6f69757a644b4d4479431d566aa693
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-04 12:05:16 +0300

    Different example

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 /**
  * 
- * @param {num} num 
+ * @param {x} x 
+ * @param {y} y 
+ * @param {z} z 
  * @returns {boolean}
  */
-const checkNum = function (num) {
-  return num > 20 && num % 7 === 0;
+const triangleExistenceTest = function (x=3, y=4, z=5){
+  return x+y>z && x+z>y && z+y>x;
 }
-console.log(checkNum(0));
+console.log(triangleExistenceTest(1,1,10));
+
+
+//---------------------------------//
+
+
+const calcVolumeSfera = function(radius=100, PI=3.14){
+  if(radius<=0){
+    return NaN;
+  }
+  return 4*PI*(radius**3)/3;
+}
+console.log(calcVolumeSfera(23))
+
+//---------------------------------//
+
+
+const returnFunction = function(){
+  const input = prompt('Type something')
+  if(input){
+    return 'Your input'+input;
+  }
+  return 'Repeat input';
+}
+console.log(returnFunction())
+
+
+//---------------------------------//
+
+
+// const returnFunction = function(){
+//   const input = prompt('Type something')
+//   if(input==='' || input===null){
+//     return 'Repeat input';
+//   }
+//   return 'Your input'+input;
+// }
+// console.log(returnFunction())
+  
