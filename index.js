fc83786cccc688542eb847e307cbdf790d6b559e
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-04 10:00:30 +0300

    Example isAdult

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const sideSquare = prompt('enter side square' , '10')
 /**
  * 
- * @param {number} side 
- * @returns {null | number}
+ * @param {number} age 
+ * @returns {boolean}
  */
-const calcPerimetrSquare = function(side=0){
-  if(side<0){
-    return null;
-  }
-  return side*4;
+const isAdult = function(age=0, ADULT=18){
+  return age>=ADULT;
 }
-console.log(calcPerimetrSquare()) //NaN
-console.log(calcPerimetrSquare(sideSquare))  //40
-console.log(calcPerimetrSquare(8))  //32
+const result = isAdult(18, 21);
+console.log(result);
 
