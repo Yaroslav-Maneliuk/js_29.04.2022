321831d1cbe26931e47f38731039ff3d1a4fb08b
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-05 10:09:41 +0300

    Example used ternarny operator

--- head.js
+++
@@ -1,5 +0,0 @@
-const sideSquare = prompt('enter side square' , '10')
-const calcPerimetrSquare = function(){
-  return sideSquare*4;
-}
-console.log(calcPerimetrSquare)
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 //Conditional (ternarny) operator
 
 //true|false
-const isEven = function(number=432){
-  return number%2 === 0;
-}
+// const isEven = function(number=432){
+//   return number%2 === 0;
+// }
+
+//---------------//
 
 //'even'|'odd'
-const returnEvenOrOdd = function(number=432){
-  // if(number%2 === 0){
-  //   return 'even';
-  // }
-  // return 'odd';
-  return (number%2 === 0) ? 'even' : 'odd';
-}
+// const returnEvenOrOdd = function(number=432){
+//   // if(number%2 === 0){
+//   //   return 'even';
+//   // }
+//   // return 'odd';
+//   return (number%2 === 0) ? 'even' : 'odd';
+// }
+
+//---------------//
+
+// const checkAge = function(age){
+//   return (age>18) ? 'adult' : 'not adult';
+// }
+
+//---------------//
+
+const summa = 123456;
+const LIMIT_SUMMA = 10000;
+
+const sale = summa > LIMIT_SUMMA ? 0.1 : 0.02;
+
+// const classInput = isValid ? 'valid' : 'invalid'
+
