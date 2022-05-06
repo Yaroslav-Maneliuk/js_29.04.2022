c5e753c19a1a7d00a31222cb8ca538a5617d32df
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-06 11:17:53 +0300

    Example password while(true)

--- index.js
+++ index.js
@@ -1,50 +1,14 @@
-
-//[1,2,12) - winter
-//[3,4,5) - spring
-//[6,7,8) - summer
-//[9-10-11) - autumn
-
-const numberMonth = '8';
-if (isNaN(numberMonth) || numberMonth <= 0){
-  console.log('error')
-} else if(numberMonth <=2){
-  console.log('winter')
-} else if(numberMonth <=5){
-  console.log('spring')
-} else if(numberMonth <=8){
-  console.log('summer')
-} else if(numberMonth <=11){
-  console.log('autumn')
-} else if(numberMonth <=12){
-  console.log('winter')
-} else {
-  console.log('fail')
+let count = 0;
+while(true){
+  const userInput = prompt('enter password', '***');
+  count++;
+  alert('try = ' + count);
+  if(count >= MAX_TRY){
+    alert('try end');
+    break;
+  }
+  if(userInput === CORRECT_PASSWORD){
+    alert('welcome');
+    break;
+  }
 }
-
-
-// const numberMonth = prompt("Choose number: ");
-// switch (numberMonth) {
-//   case "1":
-//   case "2":
-//   case "12":
-//     console.log("winter");
-//     break;
-//   case "3":
-//   case "4":
-//   case "5":
-//     console.log("spring");
-//     break;
-//   case "6":
-//   case "7":
-//   case "8":
-//     console.log("summer");
-//     break;
-//   case "9":
-//   case "10":
-//   case "11":
-//     console.log("autumn");
-//     break;
-//   default:
-//     console.log("repeat choose");
-//     break;
-// }
